a36316fa0cbc2fe433518172ef86c21f54c6d17a
breadly7 <marco.2000@icloud.com>
2022-02-03 14:07:43 +0100

    Some width & padding examples

--- src/main/kotlin/ch/bfh/kotlin/experiments/Responsive.kt
+++ src/main/kotlin/ch/bfh/kotlin/experiments/Responsive.kt
@@ -22,13 +22,24 @@ fun main(args: Array<String>) {
 class Responsive : App(ResponsiveView::class) {
     override fun start(stage: Stage) {
         stage.width = 200.0
-        stage.height = 200.0
+        stage.height = 300.0
         super.start(stage)
     }
 }
 
 class ResponsiveView : View("Look at this Styling") {
+    var thirtyPercentWidth = 200.0 / 3
 
+    val thirtyButton = button("30% (refresh)") {
+        prefWidth = thirtyPercentWidth
+    }
+
+    init {
+        primaryStage.widthProperty().addListener {_, _, newVal ->
+            thirtyPercentWidth = newVal.toDouble() / 3
+            thirtyButton.prefWidth = thirtyPercentWidth
+        }
+    }
 
     override val root = vbox(20, alignment = Pos.CENTER) {
         spacing = 10.0
@@ -47,6 +58,7 @@ class ResponsiveView : View("Look at this Styling") {
         button("50% (no refresh)") {
             prefWidth = primaryStage.width/2
         }
+        this += thirtyButton
     }
 
 }
