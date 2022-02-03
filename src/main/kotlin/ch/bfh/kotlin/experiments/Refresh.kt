048180d094b429ed2cdddb50e8687562a16186fd
flobo <flo.borter@gmail.com>
2022-02-03 13:52:47 +0100

    Mirror input in a label and example of a history

--- src/main/kotlin/ch/bfh/kotlin/experiments/Refresh.kt
+++ src/main/kotlin/ch/bfh/kotlin/experiments/Refresh.kt
@@ -1,14 +1,7 @@
 package ch.bfh.kotlin.experiments
 
-import javafx.beans.Observable
 import javafx.beans.property.SimpleStringProperty
-import javafx.geometry.Insets
 import javafx.geometry.Pos
-import javafx.scene.Parent
-import javafx.scene.control.TextField
-import javafx.scene.paint.Color
-import javafx.scene.text.FontPosture
-import javafx.scene.text.FontWeight
 import javafx.stage.Stage
 import tornadofx.*
 
@@ -19,16 +12,25 @@ fun main(args: Array<String>) {
 
 class Refresh : App(RefreshView::class) {
     override fun start(stage: Stage) {
-        stage.width = 200.0
-        stage.height = 200.0
+        stage.width = 300.0
+        stage.height = 500.0
         super.start(stage)
     }
 }
 
-class RefreshView : View("Look at this Styling") {
+class RefreshView : View("Refreshing view") {
     var superImportantValue1 = "Ohh"
     var superImportantValue2 = SimpleStringProperty("Ohh")
-
+    val inputProperty = SimpleStringProperty()
+    val outputProperty = inputProperty.stringBinding { dosth(it ?: "") }
+    val list = mutableListOf(
+        SimpleStringProperty("1"),
+        SimpleStringProperty("2"),
+        SimpleStringProperty("3"),
+        SimpleStringProperty("4"),
+        SimpleStringProperty("5")
+    )
+    var nextElement = 6
 
     override val root = vbox(20, alignment = Pos.CENTER) {
         val label1 = label(superImportantValue1) {
@@ -46,6 +48,37 @@ class RefreshView : View("Look at this Styling") {
                 superImportantValue2 += "h"
             }
         }
+
+        textfield(inputProperty)
+
+        label(inputProperty)
+
+        label {
+            textProperty().bind(outputProperty)
+        }
+
+        button(inputProperty) //bind to whatever you want :D
+
+        button("add next i") {
+            action { addHistoryItem(nextElement++) }
+        }
+
+        vbox {
+            //sorting first by value then by isBound for demonstration purpose
+            for (valueBinding in list.sortedWith(compareBy<SimpleStringProperty> { it.value }.thenBy { it.isBound })
+                .reversed().asObservable()) {
+                label(valueBinding)
+            }
+        }
+    }
+
+    fun dosth(string: String) = "dosth with value in fun, val: '$string'"
+
+    fun addHistoryItem(intvalue: Int) {
+        for (i in 0..3) {
+            list[i].value = list[i + 1].value
+        }
+        list[4].value = intvalue.toString()
     }
 
 }
