153eba6a851d1d823786617d925a902fa78c70fb
u233241 <florian.borter2@sbb.ch>
2022-03-04 11:35:37 +0100

    exercise 4,5

--- src/main/kotlin/ch/bfh/kotlin/experiments/databases/Excercise2.kt
+++ src/main/kotlin/ch/bfh/kotlin/experiments/databases/Excercise2.kt
@@ -1,20 +1,17 @@
 package ch.bfh.kotlin.experiments.databases
 
 fun main() {
-    val l1 = mutableListOf<Int>()
+
+}
+
+/* val l1 = mutableListOf<Int>()
     val l2 = mutableListOf<Int>()
     l1.addAll(100000000 downTo 0)
     l2.addAll(100000000 downTo 0)
     val start = System.currentTimeMillis()
     val res = same(l1, l2)
     val end = System.currentTimeMillis()
-    println("same: $res time: ${end - start}ms")
-    /*val start2 = System.currentTimeMillis()
-    val res2 = same2(l1, l2)
-    val end2 = System.currentTimeMillis()
-    println("same2: $res2 time2: ${end2 - start2}ms")*/
-}
-
+    println("same: $res time: ${end - start}ms")*/
 fun same(l1: List<Int>, l2: List<Int>): Boolean {
     if (l1.size != l2.size) {
         return false
@@ -32,7 +29,7 @@ fun same(l1: List<Int>, l2: List<Int>): Boolean {
     return true
 }
 
-//Same2 is slow AF
+//same2 is the same as same but is slow AF
 fun same2(l1: MutableList<Int>, l2: MutableList<Int>): Boolean {
     if (l1.size != l2.size) {
         return false
@@ -71,4 +68,36 @@ fun union(arr1: BooleanArray, arr2: BooleanArray): BooleanArray {
         unionArr[i] =  arr1.getOrElse(i){false} || arr2.getOrElse(i){false}
     }
     return unionArr
+}
+
+
+/*val arr1 = arrayOf(booleanArrayOf(true, false, false), booleanArrayOf(false, true, false))
+    val arr2 = arrayOf(booleanArrayOf(true, false, false), booleanArrayOf(true, false, true), booleanArrayOf(true, false, true, true))
+    compose(arr1, arr2)*/
+fun compose(arr1: Array<BooleanArray>, arr2: Array<BooleanArray>): Unit {
+    for (i in arr1.indices) {
+        for (j in arr1[i].indices) {
+                for (k in arr2[j].indices) {
+                    if (arr1[i][j] && arr2[j][k]) {
+                        println("i: $i und k: $k sind durch j: $j verbunden")
+                    }
+                }
+        }
+    }
+}
+
+/*val a1 = listOf(Pair(1, 2),Pair(1, 3),Pair(2, 1),Pair(1, 4))
+  val a2 = listOf(Pair(2, 5),Pair(5, 3),Pair(1, 7),Pair(1, 5))
+  compose2(a1, a2)*/
+fun compose2(arr1: List<Pair<Int, Int>>, arr2: List<Pair<Int, Int>>): MutableList<Pair<Int, Int>> {
+    val res = mutableListOf<Pair<Int, Int>>()
+    for (i in arr1.indices) {
+        for (j in arr2.indices) {
+            if (arr1[i].second == arr2[j].first) {
+                println("composition found: (${arr1[i].first}, ${arr2[j].second})")
+                res.add(Pair(arr1[i].first, arr2[j].second))
+            }
+        }
+    }
+    return res
 }
