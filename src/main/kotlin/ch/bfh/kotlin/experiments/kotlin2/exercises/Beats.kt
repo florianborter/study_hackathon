925f40aed2d18c1355e3776487d7c1be23c98457
u233241 <florian.borter2@sbb.ch>
2022-05-13 11:19:24 +0200

    creating music

---
+++ src/main/kotlin/ch/bfh/kotlin/experiments/kotlin2/exercises/Beats.kt
@@ -0,0 +1,50 @@
+package ch.bfh.kotlin.experiments.kotlin2.exercises
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.io.File
+import javax.sound.sampled.AudioSystem
+
+fun playSound(file: String) {
+    println("play $file")
+    val clip = AudioSystem.getClip()
+    val audioInputStream = AudioSystem.getAudioInputStream(File(file))
+    clip.open(audioInputStream)
+    clip.start()
+}
+
+suspend fun playBeat(beat: String, beatFile: String) {
+    for (index in beat.indices) {
+        if (beat.get(index) == 'x') {
+            run {
+                playSound(beatFile)
+            }
+        }
+        delay(100)
+    }
+}
+
+fun playBeats(beats: List<String>, beatFiles: List<String>) {
+    runBlocking {
+        for (index in beats.indices) {
+            launch(Dispatchers.Default) {
+                playBeat(beats[index], beatFiles[index])
+            }
+        }
+    }
+}
+
+fun main() {
+    val dir = "src/main/resources/audio/"
+    val beats = mutableListOf<String>()
+    val beatFiles = mutableListOf<String>()
+    beats.add("x-x-x-x-x-x-x-x-x-")
+    beatFiles.add(dir + "toms.aiff")
+
+    beats.add("x-----x-----x-----")
+    beatFiles.add(dir + "crash_cymbal.aiff")
+
+    playBeats(beats, beatFiles)
+}
