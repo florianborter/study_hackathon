b5b9e774d7db1fdc96c58b5aab3890a19f75f026
flobo <flo.borter@gmail.com>
2022-05-20 10:50:30 +0200

    Grep.kt

---
+++ src/main/kotlin/ch/bfh/kotlin/experiments/kotlin2/exercises/Grep.kt
@@ -0,0 +1,28 @@
+package ch.bfh.kotlin.experiments.kotlin2.exercises
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.io.File
+import kotlin.io.path.Path
+
+fun grep(grepString: String, pathString: String) {
+    runBlocking {
+        File(pathString).walkTopDown().forEach {
+            launch(Dispatchers.Default) {
+                println("started a coroutine")
+                it.readLines().forEachIndexed { index, line ->
+                    if (line.contains(grepString)) {
+                        println("found '$grepString' in file '${it.name}' on line number $index")
+                    }
+                }
+            }
+        }
+    }
+}
+
+fun main() {
+    val dir = Path("src/main").toAbsolutePath()
+    println(dir)
+    grep("font", dir.toString())
+}
