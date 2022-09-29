d3bc5d4aba862d80e51e50c6788ad8b613081b87
Pascal Cornu <pascal.cornu@gmx.ch>
2022-09-29 14:40:55 +0200

    ex2 good solution

--- src/main/c/projectAndTraining2/block1/ex2/ex2.c
+++ src/main/c/projectAndTraining2/block1/ex2/ex2.c
@@ -3,18 +3,37 @@
 
 int max(int a, int b) { return (a > b) ? a : b; }
 
-int backpack(int values[], int weights[], int capacity, int n)
+int **generateTable(int n, int capacity)
 {
-    if (capacity == 0 || n == 0) return 0;
-    if (weights[n - 1] > capacity)
+    int **calculatedValues = (int **)malloc(n * sizeof(int *));
+    for (int i = 0; i < n; i++)
     {
-        return backpack(values, weights, capacity, n - 1);
-    } 
+        calculatedValues[i] = (int *)malloc((capacity + 1) * sizeof(int));
+        for (int j = 0; j <= capacity; j++)
+        {
+            calculatedValues[i][j] = -1;
+        }
+    }
+    return calculatedValues;
+}
+
+int backpack(int values[], int weights[], int capacity, int n, int **calculatedValues)
+{
+    if (n < 0 || capacity < 0)
+        return 0;
+    if (calculatedValues[n][capacity] != -1)
+        return calculatedValues[n][capacity];
+    if (weights[n] > capacity)
+    {
+        calculatedValues[n][capacity] = backpack(values, weights, capacity, n - 1, calculatedValues);
+        return calculatedValues[n][capacity];
+    }
     else
     {
-        return max(
-            values[n - 1] + backpack(values, weights, capacity - weights[n - 1], n - 1),
-            backpack(values, weights, capacity, n - 1));
+        calculatedValues[n][capacity] = max(
+            values[n] + backpack(values, weights, capacity - weights[n], n - 1, calculatedValues),
+            backpack(values, weights, capacity, n - 1, calculatedValues));
+        return calculatedValues[n][capacity];
     }
 }
 
@@ -24,6 +43,6 @@ int main(void)
     int values[] = {6, 3, 5, 4, 4, 2};
     int weights[] = {2, 3, 7, 4, 4, 2};
     int n = sizeof(values) / sizeof(values[0]);
-    printf("%d\n", backpack(values, weights, capacity, n));
+    printf("%d\n", backpack(values, weights, capacity, n - 1, generateTable(n, capacity)));
     return 0;
 }
