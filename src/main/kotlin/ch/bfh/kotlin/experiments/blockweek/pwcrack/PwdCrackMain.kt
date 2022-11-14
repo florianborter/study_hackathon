4d907d7c2abf7796d640fe784e7e34518b7a6597
Florian Borter <flo.borter@gmail.com>
2022-11-14 13:51:29 +0100

    initial commit for PwdCrack

---
+++ src/main/kotlin/ch/bfh/kotlin/experiments/blockweek/pwcrack/PwdCrackMain.kt
@@ -0,0 +1,15 @@
+/*
+ * Main program of Pass-Crack exercise
+ */
+
+package ch.bfh.kotlin.experiments.blockweek.pwcrack
+
+import akka.actor.typed.ActorSystem
+
+object PwdCrackMain {
+
+	 @JvmStatic
+	fun main(args: Array<String>) {
+		println("PwdCrack Exercise");
+	}
+}
