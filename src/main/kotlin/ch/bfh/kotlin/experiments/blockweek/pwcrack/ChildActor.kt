45638b1e05798a98ad4496cf72dba077139c83d0
Florian Borter <flo.borter@gmail.com>
2022-11-14 16:07:23 +0100

    finished a child

--- src/main/kotlin/ch/bfh/kotlin/experiments/blockweek/pwcrack/ChildActor.kt
+++ src/main/kotlin/ch/bfh/kotlin/experiments/blockweek/pwcrack/ChildActor.kt
@@ -6,13 +6,12 @@ import akka.actor.typed.javadsl.AbstractBehavior
 import akka.actor.typed.javadsl.ActorContext
 import akka.actor.typed.javadsl.Behaviors
 import akka.actor.typed.javadsl.Receive
-import java.io.BufferedReader
 import java.io.FileReader
 
 const val PASSWORDS_FILENAME = "/blockweek/xato-net-10-million-passwords.txt"
 const val HASHES_FILENAME = "/blockweek/out.txt"
 
-class ChildActor(context: ActorContext<Message>): AbstractBehavior<Message>(context) {
+class ChildActor(context: ActorContext<Message>) : AbstractBehavior<Message>(context) {
     var offset = 0
     var size = 0
     lateinit var rootActorRef: ActorRef<Message>
@@ -29,19 +28,42 @@ class ChildActor(context: ActorContext<Message>): AbstractBehavior<Message>(cont
 
     private fun crack(message: StartCrackMessage): Behavior<Message> {
         println("Hello I'm crackin'")
+        val myHashes = readHashes(message.offset, message.size)
+        val passwords = readPasswords()
+
+        for (pw in passwords) {
+            for (hash in myHashes) {
+                val hashedPw = DecryptPasswords.hash("sha-512", pw + hash.salt)
+                if (hash.hash == hashedPw) {
+                    rootActorRef.tell(ReportCrackSuccess(username = hash.user, password = pw))
+                }
+            }
+        }
+
         return this
     }
 
-    private fun readHashes(offset: Int, size: Int) {
-        BufferedReader(FileReader(ChildActor::class.java.getResource(HASHES_FILENAME)?.path ?: "" )).use {
-
+    private fun readHashes(offset: Int, size: Int): List<HashEntry> {
+        val inFile = FileReader(ChildActor::class.java.getResource(HASHES_FILENAME)?.path ?: "")
+        val lines = inFile.readLines()
+        val effectiveLines = lines.subList(offset, offset + size)
+        return effectiveLines.map {
+            val splitLine = it.split(" ")
+            HashEntry(splitLine[0], splitLine[1], splitLine[2])
         }
-
     }
+
+    private fun readPasswords(): List<String> {
+        val inFile = FileReader(ChildActor::class.java.getResource(PASSWORDS_FILENAME)?.path ?: "")
+        return inFile.readLines()
+    }
+
 
     companion object {
         fun create(): Behavior<Message> {
-            return Behaviors.setup{ ChildActor(it) }
+            return Behaviors.setup { ChildActor(it) }
         }
     }
 }
+
+class HashEntry(val user: String, val salt: String, val hash: String)
--- src/main/kotlin/ch/bfh/kotlin/experiments/blockweek/pwcrack/PWMessage.kt
+++ src/main/kotlin/ch/bfh/kotlin/experiments/blockweek/pwcrack/PWMessage.kt
@@ -4,6 +4,8 @@ import akka.actor.typed.ActorRef
 
 interface Message
 
-class StartCrackMessage(val offset: Int, val size: Int, val ref: ActorRef<Message>): Message
+class StartCrackMessage(val offset: Int, val size: Int, val ref: ActorRef<Message>) : Message
 
 class InitCrackMessage : Message
+
+class ReportCrackSuccess(val username: String, val password: String): Message
