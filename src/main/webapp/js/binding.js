d3618432be92c664e16d3c378dbe423fd9c4ab62
flobo <flo.borter@gmail.com>
2023-01-24 19:06:49 +0100

    todo SPA läuft

--- src/main/webapp/js/binding.js
+++ src/main/webapp/js/binding.js
@@ -2,10 +2,8 @@ export default {
 	bind: function(view, object) {
 		view.querySelectorAll('input').forEach(element => {
 			let name = element.getAttribute('name');
-			if (object[name]) {
-				element.value = object[name];
-				element.onchange = event => object[name] = element.value;
-			}
+			element.value = object[name] || '';
+			element.onchange = event => object[name] = element.value;
 		});
 	}
 };
