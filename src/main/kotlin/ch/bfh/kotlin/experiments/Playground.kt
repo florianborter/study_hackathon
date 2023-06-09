7a136d4045e5e2875997dda3b54a907b9d938dda
u233241 <florian.borter2@sbb.ch>
2023-06-09 15:47:03 +0200

    some test stuff

--- src/main/kotlin/ch/bfh/kotlin/experiments/Playground.kt
+++ src/main/kotlin/ch/bfh/kotlin/experiments/Playground.kt
@@ -19,10 +19,23 @@ class PlaygroundMainView : View("Playground") {
 }
 
 fun main(args: Array<String>) {
-    launch<Playground>(args)
+    //launch<Playground>(args)
     playInPlayground(args)
 }
 
 fun playInPlayground(args: Array<String>) {
     println("you are playing in the Playground :D")
+
+    val hexString = "45 00 02 54 2A 00 20 00 29 11 7A ED 0A 00 00 02 C0 A8 00 02"
+    val splitHexString = hexString.split(" ")
+    println("version / ihl " + splitHexString[0])
+    println("Type of service" + splitHexString[1])
+    println("Total length" + splitHexString[2] + splitHexString[3])
+    println("Fragemnt ID" + splitHexString[4] + splitHexString[5])
+    println("Fragment info" + splitHexString[6] + splitHexString[7])
+    println("ttl" + splitHexString[8])
+    println("protocol" + splitHexString[9])
+    println("checksum" + splitHexString[10] + splitHexString[11])
+    println("source address" + splitHexString[12] + splitHexString[13] + splitHexString[14] + splitHexString[15])
+    println("destination address" + splitHexString[16] + splitHexString[17] + splitHexString[18] + splitHexString[19])
 }
